397671665aaa7071410d0e9250835a07b66bf391
fajrulaslim <fajrul.aslim@gmail.com>
2022-07-16 09:12:01 +0700

    add animation load

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,6 +38,18 @@ module.exports = {
       borderRadius: {
         50: '50px',
         200: '200px',
+      },
+      animation: {
+        fadeIn: "fadeIn 2s ease-in forwards"
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 }
+        }
+      },
+      variants: {
+        animation: ["motion-safe"]
       }
     },
   },
