8f658a43bc7687fbbf945c79b1ad84686990b511
fajrulaslim <fajrul.aslim@gmail.com>
2022-07-18 21:14:49 +0700

    animasi

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,13 +46,43 @@ module.exports = {
         200: '200px',
       },
       animation: {
-        fadeIn: "fadeIn 2s ease-in forwards"
+        fadeIn: "fadeIn 1.2s ease-in forwards",
+        fadeInUp: "fadeInUp 1.2s ease-in forwards",
+        fadeInDown: "fadeInDown 1.2s ease-in forwards",
+        fadeInLeft: "fadeInLeft 1.2s ease-in forwards",
+        fadeInRight: "fadeInRight 1.2s ease-in forwards",
+        zoomIn: "zoomIn 1.2s ease-in forwards",
+        rotateIn: "rotateIn 1.2s ease-in forwards",
       },
       keyframes: {
         fadeIn: {
           "0%": { opacity: 0 },
           "100%": { opacity: 1 }
-        }
+        },
+        fadeInUp: {
+          "0%": { opacity: 0, transform: 'translateY(60px)' },
+          "100%": { opacity: 1, transform: 'translateY(0)' }
+        },
+        fadeInDown: {
+          "0%": { opacity: 0, transform: 'translateY(-60px)' },
+          "100%": { opacity: 1, transform: 'translateY(0)' }
+        },
+        fadeInLeft: {
+          "0%": { opacity: 0, transform: 'translateX(-60px)' },
+          "100%": { opacity: 1, transform: 'translateX(0)' }
+        },
+        fadeInRight: {
+          "0%": { opacity: 0, transform: 'translateX(60px)' },
+          "100%": { opacity: 1, transform: 'translateX(0)' }
+        },
+        zoomIn: {
+          "0%": { opacity: 0, transform: 'scale(0.5)' },
+          "100%": { opacity: 1, transform: 'scale(1)' }
+        },
+        rotateIn: {
+          "0%": { opacity: 0, transform: 'rotate(-180deg)' },
+          "100%": { opacity: 1, transform: 'rotate(0deg)' }
+        },
       },
       variants: {
         animation: ["motion-safe"]
